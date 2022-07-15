141b0cc5e627a3a32b58a121c7fa889d3a20061a
axkeyz <xahkun@gmail.com>
2022-07-15 22:10:03 +1200

    Add username column

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -32,6 +32,7 @@ class UserFactory extends Factory
 
         return [
             'name' => fake()->name(),
+            'username' => fake()->userName(),
             'email' => fake()->safeEmail(),
             'email_verified_at' => $email_verified_at,
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
